ad9105d2511259a47b904b2a3b9d7838c5b06bf3
murfdog64 <cmfarris7@gmail.com>
2021-11-03 11:50:34 -0600

    added about page styling and initialized footer

---
+++ about.php
@@ -0,0 +1,23 @@
+<head>
+  <link rel="stylesheet" href="styles.css">
+  <link href="https://fonts.googleapis.com/css2?family=Rubik&family=Mate+SC" rel="stylesheet">
+</head>
+<body>
+  <?php include "./nav.html" ?>
+  <div class="about-landing">
+    <h1>The Race</h1>
+
+    <p>Since the genesis of the race, the directors felt that course should be experienced in each and every way. To make this happen a move was made, the likes of which has never been done. In even years the loop is run clockwise, while in odd years the course switches directions and is run counter clockwise. Just one of the many innovative aspects that makes Lambert unlike any other competition.</p>
+
+    <p>Racers will sprint through the majestic preserves that create a priceless oasis amid the mansions of Utah’s finest personal injury attorneys and multi level marketers. Climbing and turning and descending through legendary trails such as, High Bench, Spring, Middle Spring and Spring. There are also other trails for which to run on. </p>
+
+    <h2>Maps</h2>
+    <iframe src="https://ayvri.com/embed/7j0weq2dky/ckv8qydxv00013h6cnsph428u" allowfullscreen    height="500" width="700">
+    </iframe>
+    <br>
+    <iframe src="/images/leaflet.html" frameborder="0" height="500" width="700"></iframe>
+    <h2>Elevation Profile</h2>
+    <img src="images/elv-profile.png" alt="">
+  </div>
+  <?php include "./footer.html" ?>
+</body>
